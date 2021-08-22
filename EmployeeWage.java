4c11b18a434058c6cc89b6a9248695e51a4164b7
Your Name <you@example.com>
2021-08-22 17:34:45 +0530

    Added: Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,16 @@
 package EmployeeWages;
 
 public class EmployeeWage {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome employers");
+		//Constant
+		int emp_time = 1;
+		//Obtaining random values
+		double checkemp = Math.floor(Math.random() * 10) % 2;
+		if (checkemp == emp_time) 
+			System.out.println("Employee is present");
+		else 
+			System.out.println("Employee is absent");
+			
 
 	}
 
