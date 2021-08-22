53a452b0bc5e33a6b139d3ee2d6da5f976a119b9
Your Name <you@example.com>
2021-08-22 17:44:56 +0530

    Added: Switch Statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,24 +1,34 @@
 package EmployeeWages;
 
 public class EmployeeWage {
+	public static final int Is_full_time = 2;
+	public static final int Is_Part_time = 1;
+	public static final int Emp_Rate_Perhr = 20;
+	
+	
 	public static void main(String[] args) {
-		//Constants
-		int Is_full_time = 2;
-		int Is_Part_time = 1;
-		int Emp_Rate_Perhr = 20;
-		//Variable
+		//variables
 		int emphr = 0;
 		int empwage=0;
 		
-		double checkemp = Math.floor(Math.random() * 10) %3;
-		if (checkemp == Is_full_time) 
-			emphr =8;
-		else if (checkemp == Is_Part_time) 
-			emphr=4; 
-		else 
-			emphr =0;
+		int checkemp =(int) Math.floor(Math.random() * 10) % 3;
+		switch (checkemp) {
+		case Is_full_time:
+			emphr=8; 
+			break;
+		case Is_Part_time:
+			emphr=8; 
+		    break;
+		default:
+			emphr=0;
+			break;
+		    	
+		}
+		
+		
 		empwage = emphr * Emp_Rate_Perhr;
 		System.out.println("Employee wage" +empwage);
+
 
 	}
 
