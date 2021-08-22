640076df9c90ae6e14c2758fe014fa62dbeb93ff
Your Name <you@example.com>
2021-08-22 17:40:41 +0530

    Added: Part Time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,20 +3,22 @@ package EmployeeWages;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		//Constants
-		int Is_full_time = 1;
+		int Is_full_time = 2;
+		int Is_Part_time = 1;
 		int Emp_Rate_Perhr = 20;
-		//Variables
+		//Variable
 		int emphr = 0;
 		int empwage=0;
 		
-		double checkemp = Math.floor(Math.random() * 10) % 2;
+		double checkemp = Math.floor(Math.random() * 10) %3;
 		if (checkemp == Is_full_time) 
 			emphr =8;
+		else if (checkemp == Is_Part_time) 
+			emphr=4; 
 		else 
-			emphr=0; 
+			emphr =0;
 		empwage = emphr * Emp_Rate_Perhr;
 		System.out.println("Employee wage" +empwage);
-		
 
 	}
 
