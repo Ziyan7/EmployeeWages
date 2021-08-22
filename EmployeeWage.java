9a341df07d19e3df44f5fd58b13578a1c41158b9
Your Name <you@example.com>
2021-08-22 17:58:12 +0530

    Used Objects

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,45 +1,58 @@
 package EmployeeWages;
 
-
-
-public class EmployeeWage {
-
-	public static final int Is_full_time = 2;
-	public static final int Is_Part_time = 1;
-	public static final int Emp_Rate_Perhr = 20;
-	public static final int No_days = 20;
-	public static final int Max_No_hours = 100;
-	public static final int Max_No_days = 20;
+public class EmployeeWage 
+{
+	//instance variables
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME =2;
+	private final String company;
+	private final int EMP_RATE_PER_HOUR;
+	private final int NUM_WORKING_DAYS;
+	private final int MAX_HRS_IN_MONTH;
 	
-	public static void main(String[] args) {
+	//constructor 
+	public EmployeeWage (String company,int EMP_RATE_PER_HOUR,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH)
+	{
+		this.company=company;
+		this.EMP_RATE_PER_HOUR=EMP_RATE_PER_HOUR;
+		this.NUM_WORKING_DAYS = NUM_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
 		
-		int emphr = 0;
-		int tothours=0;
-		int totdays=0;
-		int totalempsalary=0;
-		
-		while (tothours<Max_No_hours && totdays<Max_No_days) {
-			totdays++;	
-		
-			int checkemp =(int) Math.floor(Math.random() * 10) % 3;
-			switch (checkemp) {
-			case Is_full_time:
-				emphr=8; 
-				break;
-			case Is_Part_time:
-				emphr=8; 
-				break;
-			default:
-				emphr=0;
-				break;
-		    	
-			}
-		tothours+=emphr;
-		System.out.println("hours" +tothours + "days" +totdays);
-		}
-		totalempsalary = tothours * Max_No_hours;
-		
-		System.out.println("total wage" +totalempsalary);
 	}
-
+	public static void main(String[] args) 
+	{
+		//local variables
+		int Emp_Hrs=0;
+		int Emp_Wage=0;
+		int Total_Wage=0;
+		int TotalEmpHrs=0;
+		int TotalWorkingDays=0;
+		
+		 //object creation
+		EmployeeWage obj1 = new EmployeeWage("YML",20,20,100);      
+		
+		// Computation
+		while(TotalEmpHrs<obj1.MAX_HRS_IN_MONTH && TotalWorkingDays<obj1.NUM_WORKING_DAYS)
+		{
+			TotalWorkingDays+=1;
+			int empCheck=(int) (Math.floor(Math.random()*10)%3);
+			switch (empCheck) 
+			{
+				case  IS_FULL_TIME: 
+					Emp_Hrs=16;
+					break;
+				case IS_PART_TIME: 
+					Emp_Hrs=8;
+					break;
+				default: Emp_Hrs=0;
+			}
+			TotalEmpHrs+=Emp_Hrs;
+			Emp_Wage=Emp_Hrs*obj1.EMP_RATE_PER_HOUR ;
+			Total_Wage+=Emp_Wage;
+			System.out.println("Employee wage for the company "+obj1.company+" is "+Emp_Wage);
+		}
+		System.out.println("Total wage is "+Total_Wage);
+		System.out.println("Total working hours is "+TotalEmpHrs);
+		System.out.println("Total working days is "+TotalWorkingDays);
+	}
 }
