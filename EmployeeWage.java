b33dfe5ee6a9bab6f2c5393ad7d07c6cdeca3055
Your Name <you@example.com>
2021-08-22 17:51:47 +0530

    Added: Wages Based on Condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,38 +1,45 @@
 package EmployeeWages;
 
 
+
 public class EmployeeWage {
+
 	public static final int Is_full_time = 2;
 	public static final int Is_Part_time = 1;
 	public static final int Emp_Rate_Perhr = 20;
 	public static final int No_days = 20;
+	public static final int Max_No_hours = 100;
+	public static final int Max_No_days = 20;
 	
 	public static void main(String[] args) {
-		//Variables
+		
 		int emphr = 0;
-		int empwage=0;
-		int monthlywage=0;
+		int tothours=0;
+		int totdays=0;
+		int totalempsalary=0;
 		
-		int checkemp =(int) Math.floor(Math.random() * 10) % 3;
-		switch (checkemp) {
-		case Is_full_time:
-			emphr=8; 
-			break;
-		case Is_Part_time:
-			emphr=8; 
-		    break;
-		default:
-			emphr=0;
-			break;
+		while (tothours<Max_No_hours && totdays<Max_No_days) {
+			totdays++;	
+		
+			int checkemp =(int) Math.floor(Math.random() * 10) % 3;
+			switch (checkemp) {
+			case Is_full_time:
+				emphr=8; 
+				break;
+			case Is_Part_time:
+				emphr=8; 
+				break;
+			default:
+				emphr=0;
+				break;
 		    	
+			}
+		tothours+=emphr;
+		System.out.println("hours" +tothours + "days" +totdays);
 		}
+		totalempsalary = tothours * Max_No_hours;
 		
-		
-		empwage = emphr * Emp_Rate_Perhr;
-		monthlywage= empwage * No_days;
-		System.out.println("Employee wage per month " +monthlywage);
-
-
+		System.out.println("total wage" +totalempsalary);
 	}
 
 }
