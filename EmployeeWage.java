e7b8fcc0ee108bb579d6f923c1da17854bf34a43
Your Name <you@example.com>
2021-08-22 22:04:47 +0530

    Added: Objects for different Companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,16 +2,18 @@ package EmployeeWages;
 
 public class EmployeeWage 
 {
-	//instance variables
+	
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME =2;
+	
+	//instance Variable
 	private final String company;
 	private final int EMP_RATE_PER_HOUR;
 	private final int NUM_WORKING_DAYS;
 	private final int MAX_HRS_IN_MONTH;
 	
-	//constructor 
-	public EmployeeWage (String company,int EMP_RATE_PER_HOUR,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH)
+	//constructor
+	public EmployeeWage(String company,int EMP_RATE_PER_HOUR,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH)
 	{
 		this.company=company;
 		this.EMP_RATE_PER_HOUR=EMP_RATE_PER_HOUR;
@@ -19,40 +21,46 @@ public class EmployeeWage
 		this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
 		
 	}
-	public static void main(String[] args) 
+	//method for calculating employee wages for different companies
+	public void wageCalculator(String company,int EMP_RATE_PER_HOUR, int NUM_WORKING_DAYS, int MAX_HRS_IN_MONTH)
 	{
 		//local variables
-		int Emp_Hrs=0;
-		int Emp_Wage=0;
-		int Total_Wage=0;
-		int TotalEmpHrs=0;
-		int TotalWorkingDays=0;
+				int Emp_Hrs=0;
+				int Emp_Wage=0;
+				int Total_Wage=0;
+				int TotalEmpHrs=0;
+				int TotalWorkingDays=0;
+				while(TotalEmpHrs<MAX_HRS_IN_MONTH && TotalWorkingDays<NUM_WORKING_DAYS)
+				{
+					TotalWorkingDays+=1;
+					int empCheck=(int) (Math.floor(Math.random()*10)%3);
+					switch (empCheck) 
+					{
+						case  IS_FULL_TIME: Emp_Hrs=16;break;
+						case IS_PART_TIME: Emp_Hrs=8;break;
+						default: Emp_Hrs=0;
+					}
+					TotalEmpHrs+=Emp_Hrs;
+					Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
+					Total_Wage+=Emp_Wage;
+					System.out.println("Employee wage for the company "+company+" is "+Emp_Wage);
+				}
+				System.out.println("Total wage is "+Total_Wage);
+				System.out.println("Total working hours is "+TotalEmpHrs);
+				System.out.println("Total working days is "+TotalWorkingDays);
 		
-		 //object creation
-		EmployeeWage obj1 = new EmployeeWage("YML",20,20,100);      
+	}
+	public static void main(String[] args) 
+	{
+
 		
+		
+		EmployeeWage obj1 = new EmployeeWage("YML",20,20,100);//object creation for company 1
+		EmployeeWage obj2 = new EmployeeWage("DMart",10,20,150);//object creation for company 2
+		EmployeeWage obj3 = new EmployeeWage("Reliance",30,20,100);//object creation for company 3
 		// Computation
-		while(TotalEmpHrs<obj1.MAX_HRS_IN_MONTH && TotalWorkingDays<obj1.NUM_WORKING_DAYS)
-		{
-			TotalWorkingDays+=1;
-			int empCheck=(int) (Math.floor(Math.random()*10)%3);
-			switch (empCheck) 
-			{
-				case  IS_FULL_TIME: 
-					Emp_Hrs=16;
-					break;
-				case IS_PART_TIME: 
-					Emp_Hrs=8;
-					break;
-				default: Emp_Hrs=0;
-			}
-			TotalEmpHrs+=Emp_Hrs;
-			Emp_Wage=Emp_Hrs*obj1.EMP_RATE_PER_HOUR ;
-			Total_Wage+=Emp_Wage;
-			System.out.println("Employee wage for the company "+obj1.company+" is "+Emp_Wage);
-		}
-		System.out.println("Total wage is "+Total_Wage);
-		System.out.println("Total working hours is "+TotalEmpHrs);
-		System.out.println("Total working days is "+TotalWorkingDays);
+		obj1.wageCalculator(obj1.company,obj1.EMP_RATE_PER_HOUR,obj1.NUM_WORKING_DAYS,obj1.MAX_HRS_IN_MONTH);
+		obj2.wageCalculator(obj2.company,obj2.EMP_RATE_PER_HOUR,obj2.NUM_WORKING_DAYS,obj2.MAX_HRS_IN_MONTH);
+		obj3.wageCalculator(obj3.company,obj3.EMP_RATE_PER_HOUR,obj3.NUM_WORKING_DAYS,obj3.MAX_HRS_IN_MONTH);
 	}
 }
