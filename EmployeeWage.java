cf4e7264aed4b257785d7603609a1df6da088354
Your Name <you@example.com>
2021-08-24 22:12:16 +0530

    Added: Total Wage for each company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -11,7 +11,7 @@ public class EmployeeWage
 	private final int EMP_RATE_PER_HOUR;
 	private final int NUM_WORKING_DAYS;
 	private final int MAX_HRS_IN_MONTH;
-	
+	private int Total_Wage;
 	//constructor
 	public EmployeeWage(String company,int EMP_RATE_PER_HOUR,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH)
 	{
@@ -21,46 +21,57 @@ public class EmployeeWage
 		this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
 		
 	}
-	//method for calculating employee wages for different companies
-	public void wageCalculator(String company,int EMP_RATE_PER_HOUR, int NUM_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+	//For calculating employee wages for different companies
+	public void wageCalculator()
 	{
 		//local variables
-				int Emp_Hrs=0;
-				int Emp_Wage=0;
-				int Total_Wage=0;
-				int TotalEmpHrs=0;
-				int TotalWorkingDays=0;
-				while(TotalEmpHrs<MAX_HRS_IN_MONTH && TotalWorkingDays<NUM_WORKING_DAYS)
-				{
-					TotalWorkingDays+=1;
-					int empCheck=(int) (Math.floor(Math.random()*10)%3);
-					switch (empCheck) 
-					{
-						case  IS_FULL_TIME: Emp_Hrs=16;break;
-						case IS_PART_TIME: Emp_Hrs=8;break;
-						default: Emp_Hrs=0;
-					}
-					TotalEmpHrs+=Emp_Hrs;
-					Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
-					Total_Wage+=Emp_Wage;
-					System.out.println("Employee wage for the company "+company+" is "+Emp_Wage);
-				}
-				System.out.println("Total wage is "+Total_Wage);
-				System.out.println("Total working hours is "+TotalEmpHrs);
-				System.out.println("Total working days is "+TotalWorkingDays);
+		int Emp_Hrs=0;
+		int Emp_Wage=0;
+		int TotalEmpHrs=0;
+		int TotalWorkingDays=0;
+		while(TotalEmpHrs<MAX_HRS_IN_MONTH && TotalWorkingDays<NUM_WORKING_DAYS)
+		{
+			TotalWorkingDays+=1;
+			int empCheck=(int) (Math.floor(Math.random()*10)%3);
+			switch (empCheck) 
+			{
+				case  IS_FULL_TIME: 
+					Emp_Hrs=16;
+					break;
+				case IS_PART_TIME: 
+					Emp_Hrs=8;
+					break;
+				default: Emp_Hrs=0;
+			}
+				TotalEmpHrs+=Emp_Hrs;
+				Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
+				Total_Wage+=Emp_Wage;
+				System.out.println("Employee wage for the company "+company+" is "+Emp_Wage);
+			}
+		System.out.println("Total working hours is "+TotalEmpHrs);
+		System.out.println("Total working days is "+TotalWorkingDays);
 		
 	}
+	
+	@Override
+	public String toString() {
+		return "Total Emp Wage for compay: "+company+ "is: "+Total_Wage;
+	}
+	
 	public static void main(String[] args) 
 	{
 
 		
 		
-		EmployeeWage obj1 = new EmployeeWage("YML",20,20,100);//object creation for company 1
-		EmployeeWage obj2 = new EmployeeWage("DMart",10,20,150);//object creation for company 2
-		EmployeeWage obj3 = new EmployeeWage("Reliance",30,20,100);//object creation for company 3
+		EmployeeWage YML = new EmployeeWage("YML",20,20,100);//object creation for company 1
+		EmployeeWage DMart = new EmployeeWage("DMart",10,20,150);//object creation for company 2
+		EmployeeWage Reliance = new EmployeeWage("Reliance",30,20,100);//object creation for company 3
 		// Computation
-		obj1.wageCalculator(obj1.company,obj1.EMP_RATE_PER_HOUR,obj1.NUM_WORKING_DAYS,obj1.MAX_HRS_IN_MONTH);
-		obj2.wageCalculator(obj2.company,obj2.EMP_RATE_PER_HOUR,obj2.NUM_WORKING_DAYS,obj2.MAX_HRS_IN_MONTH);
-		obj3.wageCalculator(obj3.company,obj3.EMP_RATE_PER_HOUR,obj3.NUM_WORKING_DAYS,obj3.MAX_HRS_IN_MONTH);
+		YML.wageCalculator();
+		System.out.println(YML);
+		DMart.wageCalculator();
+		System.out.println(DMart);
+		Reliance.wageCalculator();
+		System.out.println(Reliance);
 	}
 }
