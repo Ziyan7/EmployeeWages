b0f4a32ccfb9b362749862420d1600e24232866c
Your Name <you@example.com>
2021-08-25 22:51:06 +0530

    Added: Interface Approach

---
+++ ComputeWage.java
@@ -0,0 +1,7 @@
+package EmployeeWages;
+
+
+public interface ComputeWage {
+		public void addCompanyEmpWage(String company,int EMP_RATE_PER_HOUR,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH ); //to add employee wage to each company
+		public void computeEmpWage(); //defining to compute employee wage in EmpWage class
+}
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,13 @@
 package EmployeeWages;
 
-public class EmployeeWage 
-{
-	
+public class EmployeeWage implements ComputeWage{
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME =2;
 	
 	private int numOfCompany=0;
 	private CompanyEmpWage[] companyEmpWageArray;
 	
-	//Constructor
+	//parameterised constructor for initializing members of a class
 	public EmployeeWage()
 	{
 		companyEmpWageArray=new CompanyEmpWage[5];
@@ -21,9 +19,7 @@ public class EmployeeWage
 		companyEmpWageArray[numOfCompany]=new CompanyEmpWage(company, EMP_RATE_PER_HOUR, NUM_WORKING_DAYS, MAX_HRS_IN_MONTH);
 		numOfCompany++;
 	}
-	
-	
-	private void computeEmpWage()
+	public void computeEmpWage()
 	{
 		for(int i=0;i<numOfCompany;i++)
 		{
@@ -31,43 +27,41 @@ public class EmployeeWage
 			System.out.println(companyEmpWageArray[i]);
 		}
 	}
-	//For calculating employee wages for different companies
+
 	//method for calculating employee wages for different companies
-		private int computeEmpWage(CompanyEmpWage companyEmpWage)
-		{	
+	private int computeEmpWage(CompanyEmpWage companyEmpWage)
+	{	
 		//local variables
-			int Emp_Hrs=0;
-			int TotalEmpHrs=0;
-			int TotalWorkingDays=0;
+				int Emp_Hrs=0;
+				int TotalEmpHrs=0;
+				int TotalWorkingDays=0;
 			//computation
-			while(TotalEmpHrs<=companyEmpWage.MAX_HRS_IN_MONTH && TotalWorkingDays<companyEmpWage.NUM_WORKING_DAYS)
-			{
-				TotalWorkingDays+=1;
-				int empCheck=(int) (Math.floor(Math.random()*10)%3);
-				switch (empCheck) 
+				while(TotalEmpHrs<=companyEmpWage.maxHoursInMonth && TotalWorkingDays<companyEmpWage.numOfWorkingHours)
 				{
-					case  IS_FULL_TIME: 
-					Emp_Hrs=16;
-					break;
-					case IS_PART_TIME: 
-					Emp_Hrs=8;
-					break;
-					default: Emp_Hrs=0;
+					TotalWorkingDays+=1;
+					int empCheck=(int) (Math.floor(Math.random()*10)%3);
+					switch (empCheck) 
+					{
+						case  IS_FULL_TIME: Emp_Hrs=16;break;
+						case IS_PART_TIME: Emp_Hrs=8;break;
+						default: Emp_Hrs=0;
+					}
+					TotalEmpHrs+=Emp_Hrs;
+					System.out.println("Day#: "+TotalWorkingDays+" Emp Hr: "+TotalEmpHrs);
 				}
-			TotalEmpHrs+=Emp_Hrs;
-			System.out.println("Day#: "+TotalWorkingDays+" Emp Hr: "+TotalEmpHrs);
-			}
-						
-		return TotalEmpHrs*companyEmpWage.EMP_RATE_PER_HOUR;				
-		}
-	
+					
+					return TotalEmpHrs*companyEmpWage.empRatePerHour;
+					
+	}
 	
 	public static void main(String[] args) 
 	{
 
-		EmployeeWage empWage =new EmployeeWage();
-		empWage.addCompanyEmpWage("DMart",20,2,10);
-		empWage.addCompanyEmpWage("Reliance",10,4,20);
-		empWage.computeEmpWage();
+		EmployeeWage empWageBuilder =new EmployeeWage();
+		
+		empWageBuilder.addCompanyEmpWage("YML",20,2,10);
+		empWageBuilder.addCompanyEmpWage("Dmart",10,4,20);
+		empWageBuilder.computeEmpWage();
 	}
+
 }
